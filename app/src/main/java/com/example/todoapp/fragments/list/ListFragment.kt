9cec46011b0aade2ad7d65af46b1a2a631427b03
billyyan <By869422>
2022-02-11 00:32:14 -0800

    Data Binding for ListAdapter.kt

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -1,31 +1,47 @@
 package com.example.todoapp.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoapp.R
+import com.example.todoapp.data.viewmodel.ToDoViewModel
 import com.example.todoapp.databinding.FragmentListBinding
+import com.example.todoapp.fragments.SharedViewModel
 
 class ListFragment : Fragment() {
 
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
 
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
+
+    private val adapter: ListAdapter by lazy { ListAdapter() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
+        // Data binding
         _binding = FragmentListBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mShareViewModel = mSharedViewModel
 
-        binding.floatingActionButton.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_addFragment)
-        }
+        // Setup RecycleView
+        setupRecyclerview()
 
-        binding.listLayout.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
-        }
+        // Observe LiveData
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            mSharedViewModel.checkIfDatabaseEmpty(data)
+            adapter.setData(data)
+        })
 
         // Set Menu
         setHasOptionsMenu(true)
@@ -33,12 +49,43 @@ class ListFragment : Fragment() {
         return binding.root
     }
 
+    private fun setupRecyclerview() {
+        val recyclerView = binding. recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete_all) {
+            confirmRemoval()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Show AlertDialog to Confirm Removal of All Items from Database Table
+    private fun confirmRemoval() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mToDoViewModel.deleteAll()
+            Toast.makeText(
+                requireContext(),
+                "Successfully Removed Everything!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        builder.setNegativeButton("No") { _, _ ->
+            builder.setTitle("Delete Everything?")
+            builder.setMessage("Are you sure you want to remove Everything?")
+            builder.create().show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
--- app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
@@ -1,14 +1,27 @@
 package com.example.todoapp.fragments.update
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.todoapp.R
+import com.example.todoapp.data.models.Priority
+import com.example.todoapp.data.models.ToDoData
+import com.example.todoapp.data.viewmodel.ToDoViewModel
 import com.example.todoapp.databinding.FragmentAddBinding
 import com.example.todoapp.databinding.FragmentUpdateBinding
+import com.example.todoapp.fragments.SharedViewModel
 
 class UpdateFragment : Fragment() {
 
+    private val args by navArgs<UpdateFragmentArgs>()
+
+    private val mSharedViewModel: SharedViewModel by viewModels()
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     private var _binding: FragmentUpdateBinding? = null
     private val binding get() = _binding!!
@@ -18,11 +31,15 @@ class UpdateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
+        // Data binding
+        _binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        binding.args = args
+
         // Set Menu
         setHasOptionsMenu(true)
 
-        // Inflate the layout for this fragment
-        _binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        // Spinner Item Selected Listener
+        binding.currentPrioritiesSpinner.onItemSelectedListener = mSharedViewModel.listener
 
         return binding.root
     }
@@ -31,8 +48,58 @@ class UpdateFragment : Fragment() {
         inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_save -> updateItem()
+            R.id.menu_delete -> confirmItemRemoval()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateItem() {
+        val title = binding.currentTitleEt.text.toString()
+        val description = binding.currentDescriptionEt.text.toString()
+        val getPriority = binding.currentPrioritiesSpinner.selectedItem.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(title, description)
+        if (validation) {
+            // Update Current Item
+            val updateItem = ToDoData(
+                args.currentItem.id,
+                title,
+                mSharedViewModel.parsePriority(getPriority),
+                description
+            )
+            mToDoViewModel.updateData(updateItem)
+            Toast.makeText(requireContext(), "Successfully updated!", Toast.LENGTH_SHORT).show()
+            // Navigate back
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Show AlertDialog to Confirm item Removal
+    private fun confirmItemRemoval() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mToDoViewModel.deleteItem(args.currentItem)
+            Toast.makeText(
+                requireContext(),
+                "Successfully Removed: ${args.currentItem.title}",
+                Toast.LENGTH_SHORT
+            ).show()
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }
+        builder.setNegativeButton("No"){ _, _ ->
+            builder.setTitle("Delete '${args.currentItem.title}'?")
+            builder.setMessage("Are you sure you want to remove '${args.currentItem.title}'?")
+            builder.create().show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
