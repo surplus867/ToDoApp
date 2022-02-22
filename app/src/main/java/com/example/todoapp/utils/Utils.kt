90f3df882a424ee0bfdee6502ae0c7f16d8ff6d2
billyyan <By869422>
2022-02-21 20:37:59 -0800

    Hide keyboard

---
+++ app/src/main/java/com/example/todoapp/utils/Utils.kt
@@ -0,0 +1,19 @@
+package com.example.todoapp.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+
+fun hideKeyboard(activity: Activity) {
+    val inputMethodManager =
+        activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    val currentFocusedView: View? = activity.currentFocus
+    currentFocusedView?. let {
+        inputMethodManager.hideSoftInputFromWindow(
+            currentFocusedView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS
+        )
+    }
+}
