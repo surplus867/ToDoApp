54ca9c2fc73da2cbbc097da4fce54945f935d472
billyyan <By869422>
2022-02-21 21:03:53 -0800

    Modified onDestroyView function

--- app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
@@ -72,8 +72,8 @@ class AddFragment : Fragment() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
